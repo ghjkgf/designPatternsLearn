dd0febfdcd06a3fd360d723762ddfb5eb9c07226
zsl <zhaosenlin@dbhs.com.cn>
2022-12-26 13:11:23 +0800

    Initial commit

---
+++ src/main/java/Document.java
@@ -0,0 +1,16 @@
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public interface Document {
+
+  Void put(String key, Object value);
+
+  Object get(String key);
+
+  <T> Stream<T> children(String key, Function<Map<String, Object>, T> constructor);
+}
+
